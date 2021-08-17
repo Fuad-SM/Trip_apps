04a23243702fd0acd2add84bfe6c4d5750ec7d1a
Fuad-SM <rzaki804@gmail.com>
2021-08-17 18:08:36 +0800

    membuat header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:trip_apps/pages/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,9 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:trip_apps/theme.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
@@ -8,24 +9,40 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: bgcolor,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(12)),
-                ),
-                Text(
-                  'Hello World',
-                  style: TextStyle(color: white, fontSize: 20),
-                ),
-                Image.asset('assets/images/love.png'),
-              ],
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 13, vertical: 15),
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: iconcolor),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/love.png',
+                          width: 24,
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text(
+                    'Trip',
+                    style: titleText,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
