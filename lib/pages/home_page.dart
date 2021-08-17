2ec50fc9bc8d936716a6330cf87180ee2b1c7f59
Fuad-SM <rzaki804@gmail.com>
2021-08-17 17:42:04 +0800

    update code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:trip_apps/theme.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: bgcolor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                ),
+                Text(
+                  'Hello World',
+                  style: TextStyle(color: white, fontSize: 20),
+                ),
+                Image.asset('assets/images/love.png'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
