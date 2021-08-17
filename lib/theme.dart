7bc4a168737b015505e6602c5276c618e553d511
Fuad-SM <rzaki804@gmail.com>
2021-08-17 17:40:36 +0800

    menambahkan file theme

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color bgcolor = Color(0xff252836);
+Color iconcolor = Color(0xff353340);
+Color choicecolor = Color(0xff252836);
+Color btnnext = Color(0xff6C5ECF);
+Color white = Color(0xffFFFFFF);
+Color darker = Color(0xff757686);
+
+TextStyle titleText = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500, color: white, fontSize: 24);
+TextStyle subText = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w400, color: white, fontSize: 16);
+TextStyle descText = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w400, color: white, fontSize: 12);
